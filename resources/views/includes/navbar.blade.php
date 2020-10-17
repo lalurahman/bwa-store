b668de7d32576b2fcfb82614cda852d2553addf8
Lalu Abdurrahman <34390193+lalurahman@users.noreply.github.com>
2020-10-17 14:29:37 +0800

    update migrations

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -28,11 +28,11 @@
             <a href="#" class="nav-link">Rewards</a>
         </li>
         <li class="nav-item">
-            <a href="/register.html" class="nav-link">Sign Up</a>
+            <a href="{{ route('register') }}" class="nav-link">Sign Up</a>
         </li>
         <li class="nav-item">
             <a
-            href="/login.html"
+            href="{{ route('login') }}"
             class="btn btn-success nav-link px-4 text-white"
             >Sign In</a
             >
